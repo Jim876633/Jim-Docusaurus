1da9005d7433dcbe999b201abb730427fb39b297
jim <jim30223@gmail.com>
2023-01-08 18:35:10 +0800

    feat: delete origin file and add first article

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,15 +36,15 @@ const config = {
                     sidebarPath: require.resolve("./sidebars.js"),
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl:
-                        "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                    // editUrl:
+                    //     "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
                 },
                 blog: {
                     showReadingTime: true,
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl:
-                        "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                    // editUrl:
+                    //     "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
                 },
                 theme: {
                     customCss: require.resolve("./src/css/custom.css"),
@@ -69,13 +69,14 @@ const config = {
                         position: "left",
                         label: "Note",
                     },
-                    { to: "/blog", label: "Blog", position: "left" },
+                    // { to: "/blog", label: "Blog", position: "left" },
                     {
                         href: "https://github.com/Jim876633",
                         label: "GitHub",
                         position: "right",
                     },
                 ],
+                hideOnScroll: true,
             },
             footer: {
                 style: "dark",
@@ -120,7 +121,7 @@ const config = {
                         ],
                     },
                 ],
-                copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+                copyright: `Copyright © ${new Date().getFullYear()} Jim's note, Inc. Built with Docusaurus.`,
             },
             prism: {
                 theme: lightCodeTheme,
