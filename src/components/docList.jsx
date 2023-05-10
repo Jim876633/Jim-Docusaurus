e3c024773dacb3449c49d675769e65193bce2896
Jim Huang <jim30223@gmail.com>
2023-05-10 16:55:18 +0800

    chore: use Tanstack Query to fetch docs list

--- src/components/docList.jsx
+++ src/components/docList.jsx
@@ -1,53 +1,43 @@
-import React, { useState, useEffect } from "react";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+import React from "react";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { useDocsSidebar } from "@docusaurus/theme-common/internal";
 
+const queryClient = new QueryClient();
+
 export default function () {
-  const [docList, setDocList] = useState([]);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <DocList />
+      {/* <ReactQueryDevtools /> */}
+    </QueryClientProvider>
+  );
+}
+
+function DocList() {
   const { items } = useDocsSidebar();
-  const docs = getDocList(items);
-  console.log(docs);
-  const docUrlList = docs.map(
-    (doc) => `https://jim876633.github.io${doc.href}`
-  );
-  const promises = docUrlList.map((url) =>
-    fetch(url).then((res) => res.text())
-  );
+  const docs = formatDocList(items);
 
-  useEffect(() => {
-    // Use Promise.all to wait for all promises to resolve
-    Promise.all(promises)
-      .then((results) => {
-        results.forEach((data, i) => {
-          // translate to html
-          const parser = new DOMParser();
-          const html = parser.parseFromString(data, "text/html");
-          const body = html.body;
-          const time = body.querySelector("time");
+  const { isLoading, data } = useQuery({
+    queryKey: ["docs"],
+    queryFn: () => fetchDocs(docs),
+    retry: 1,
+    refetchOnWindowFocus: false,
+    staleTime: Infinity,
+  });
 
-          if (time) {
-            const timeStamp = time.getAttribute("datetime");
-            docs[i].timeStamp = new Date(timeStamp).getTime();
-            console.log(new Date(timeStamp).toLocaleDateString("zh-TW"));
-            docs[i].date = new Date(timeStamp).toLocaleDateString("zh-TW", {
-              year: "numeric",
-              month: "2-digit",
-              day: "2-digit",
-            });
-          }
-        });
-        docs.sort((a, b) => b.timeStamp - a.timeStamp);
-        const lastTenDocs = docs.slice(0, 10);
-        setDocList(lastTenDocs);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
 
   return (
     <div>
-      {docList.map((doc) => (
+      {data.map((doc) => (
         <li key={doc.id} style={{}}>
           {doc.date} 更新文章 <a href={doc.href}>{doc.title}</a> - {doc.label}
         </li>
@@ -56,8 +46,12 @@ export default function () {
     </div>
   );
 }
-
-function getDocList(items) {
+/**
+ * format doc item from useDocsSidebar()
+ * @param {*} items
+ * @returns
+ */
+function formatDocList(items) {
   return items.flatMap((item) => {
     if (!item.items) {
       const label = item.href.split("/")[2] || "Intro";
@@ -68,7 +62,43 @@ function getDocList(items) {
         label,
       };
     } else {
-      return getDocList(item.items);
+      return formatDocList(item.items);
     }
   });
+}
+
+/**
+ * fetch docs list
+ * @param  docsList
+ * @returns
+ */
+async function fetchDocs(docsList) {
+  const docs = [...docsList];
+  const docUrlList = docsList.map(
+    (doc) => `https://jim876633.github.io${doc.href}`
+  );
+  const results = await Promise.all(
+    docUrlList.map((url) => fetch(url).then((res) => res.text()))
+  );
+
+  results.forEach((data, i) => {
+    // translate to html
+    const parser = new DOMParser();
+    const html = parser.parseFromString(data, "text/html");
+    const body = html.body;
+    const time = body.querySelector("time");
+
+    if (time) {
+      const timeStamp = time.getAttribute("datetime");
+      docs[i].timeStamp = new Date(timeStamp).getTime();
+      docs[i].date = new Date(timeStamp).toLocaleDateString("zh-TW", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      });
+    }
+  });
+  docs.sort((a, b) => b.timeStamp - a.timeStamp);
+  const lastTenDocs = docs.slice(0, 10);
+  return lastTenDocs;
 }
