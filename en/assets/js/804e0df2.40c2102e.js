7485d5a20a989cd121a5d45aa534b28078020edf
Jim876633 <jim30223@gmail.com>
2023-05-25 14:40:25 +0000

    deploy: 782c3924b866f814a334b8c0b30af6255908e4dc

---
+++ en/assets/js/804e0df2.40c2102e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[3431],{1422:e=>{e.exports=JSON.parse('{"title":"Vue2","slug":"/category/vue2","permalink":"/Jim-Docusaurus/en/category/vue2","navigation":{"previous":{"title":"Vue","permalink":"/Jim-Docusaurus/en/category/vue"},"next":{"title":"Vuex","permalink":"/Jim-Docusaurus/en/category/vuex"}}}')}}]);
