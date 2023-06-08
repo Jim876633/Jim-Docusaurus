bba497990593acd88175bc1c4cc8f007db3bd6e9
Jim Huang <jim30223@gmail.com>
2023-06-08 15:45:39 +0800

    styles: modify image width

--- src/components/Width.jsx
+++ src/components/Width.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Img = ({ src, width = "100%" }) => (
   <>
-    <img src={src} style={{ width: width }} />
+    <img src={src} style={{ width: `min(100%,${width})` }} />
     <br />
     <br />
   </>
