4b8b38fa9fb881a7e2464c600572155af69acc7a
Jim876633 <jim30223@gmail.com>
2023-11-11 12:52:01 +0000

    deploy: fbda9c81de8dc927a60f5b7f2477cbc2709f27f4

--- assets/js/e2116049.64f9c59f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[4720],{2711:e=>{e.exports=JSON.parse('{"title":"React","slug":"/category/react","permalink":"/Jim-Docusaurus/category/react","navigation":{"previous":{"title":"Intro","permalink":"/Jim-Docusaurus/"},"next":{"title":"React router","permalink":"/Jim-Docusaurus/React/congratulations"}}}')}}]);
