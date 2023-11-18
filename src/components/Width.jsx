2ed95a053d82e318b0142a8bf9865cb4ccadbffc
Jim Huang <jim.huang112@gmail.com>
2023-11-18 15:28:10 +0800

    refactor: Img component add customStyle and center

--- src/components/Width.jsx
+++ src/components/Width.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-export const Img = ({ src, width = "100%" }) => (
-  <>
-    <img src={src} style={{ width: `min(100%,${width})` }} />
-    <br />
-    <br />
-  </>
-);
+export const Img = ({ src, width = "100%", center = false, style = {} }) => {
+  const customStyle = {
+    width: `min(100%,${width})`,
+    display: center ? "block" : "inline-block",
+    margin: center ? "0 auto" : "0",
+  };
+  return (
+    <>
+      <img src={src} style={{ ...customStyle }} />
+      <br />
+      <br />
+    </>
+  );
+};
