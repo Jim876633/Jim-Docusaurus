caa4e04c592390555c51655324956b53e816870a
Jim Huang <jim.huang112@gmail.com>
2023-12-13 16:09:27 +0800

    chore: add prism additionalLanguages to fix v3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -153,6 +153,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+        additionalLanguages: ['bash', 'json', 'css', 'scss', 'python']
       },
       colorMode: {
         respectPrefersColorScheme: true,
