5aceb97ead5bd1856c31a5dfde5afe52f74865cf
Jim Huang <jim.huang112@gmail.com>
2024-02-22 15:47:16 +0800

    [chore] Add Block/ImgBlock components

--- src/components/Width.jsx
+++ src/components/Width.jsx
@@ -1,3 +1,4 @@
+import useMedia from "@site/src/hooks/useMedia.jsx";
 import React from "react";
 
 export const Img = ({ src, width = "100%", center = false, style = {} }) => {
@@ -14,5 +15,37 @@ export const Img = ({ src, width = "100%", center = false, style = {} }) => {
       <br />
       <br />
     </>
+  );
+};
+
+export const Block = ({
+  children,
+  width = "100%",
+  center = false,
+  style = {},
+}) => {
+  const { sm } = useMedia();
+  const customStyle = {
+    width: `min(100%,${width})`,
+    margin: center ? "0 auto" : "0",
+    display: "flex",
+    flexDirection: sm ? "column" : "row",
+    gap: "10px",
+    alignItems: "center",
+    ...style,
+  };
+  return <div style={{ ...customStyle }}>{children}</div>;
+};
+
+export const ImgBlock = ({ style, width = "100%", src }) => {
+  const customStyle = {
+    width: `min(100%,${width})`,
+    flex: 1,
+    ...style,
+  };
+  return (
+    <div style={{ ...customStyle }}>
+      <Img src={src} />
+    </div>
   );
 };
