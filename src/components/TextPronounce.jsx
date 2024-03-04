06a695900e6c8389fbf2970d8d6d097b8490e0bc
Jim Huang <jim.huang112@gmail.com>
2024-03-04 15:39:35 +0800

    [chore] Fix TextPronounce.jsx browserOnly

--- src/components/TextPronounce.jsx
+++ src/components/TextPronounce.jsx
@@ -1,29 +1,38 @@
 // https://developer.chrome.com/blog/web-apps-that-talk-introduction-to-the-speech-synthesis-api?hl=zh-tw
 // ISO Language Codes: http://www.lingoes.net/en/translator/langcode.htm
 import { SoundFilled } from "@ant-design/icons";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 import { useMemo, useState } from "react";
-const TextPronounce = ({ text, lang = "en-US" }) => {
-  const [slowRate, setSlowRate] = useState(false);
-  const [isVoiceEnd, setIsVoiceEnd] = useState(false);
-  const msg = useMemo(() => new SpeechSynthesisUtterance(text), [text]);
-  const playSound = async () => {
-    let rate = 0.8;
-    if (slowRate) {
-      rate = 0.5;
-    }
-    msg.rate = rate;
-    msg.lang = lang;
-    window.speechSynthesis.speak(msg);
-    setIsVoiceEnd(true);
-    setSlowRate(!slowRate);
-  };
-  msg.onend = () => {
-    setIsVoiceEnd(false);
-  };
-  return (
-    <button className='sounds_btn' onClick={playSound} disabled={isVoiceEnd}>
-      <SoundFilled />
-    </button>
-  );
-};
+const TextPronounce = ({ text, lang = "en-US" }) => (
+  <BrowserOnly>
+    {() => {
+      const [slowRate, setSlowRate] = useState(false);
+      const [isVoiceEnd, setIsVoiceEnd] = useState(false);
+      const msg = useMemo(() => new SpeechSynthesisUtterance(text), [text]);
+      const playSound = async () => {
+        let rate = 0.8;
+        if (slowRate) {
+          rate = 0.5;
+        }
+        msg.rate = rate;
+        msg.lang = lang;
+        window.speechSynthesis.speak(msg);
+        setIsVoiceEnd(true);
+        setSlowRate(!slowRate);
+      };
+      msg.onend = () => {
+        setIsVoiceEnd(false);
+      };
+      return (
+        <button
+          className='sounds_btn'
+          onClick={playSound}
+          disabled={isVoiceEnd}
+        >
+          <SoundFilled />
+        </button>
+      );
+    }}
+  </BrowserOnly>
+);
 export default TextPronounce;
